8fa1bc4a0fb26a346946acfe159d6dc0e30882ec
eugene sia <teamkronos2019@gmail.com>
2019-05-25 17:28:02 +0900

    solution

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+
 
 module RailsMisterCocktail
   class Application < Rails::Application
