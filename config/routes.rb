0723f6b8246d94d02c90894d27484f26abecbdc0
eugene sia <teamkronos2019@gmail.com>
2019-05-26 14:00:36 +0900

    redo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :new, :create, :show, :destroy ] do
-    resources :doses, only: [ :new, :create]
-  end
-  resources :doses, only: [:destroy]
-
   root to: 'cocktails#index'
+  resources :cocktails, only: [ :index, :show, :new, :create] do
+    resources :doses, only: [ :create ]
+    resources :reviews, only: [ :create ]
+  end
+  resources :doses, only: [ :destroy ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_074424) do
+ActiveRecord::Schema.define(version: 2019_05_26_035213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,13 +41,13 @@ ActiveRecord::Schema.define(version: 2019_05_25_074424) do
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rating"
-    t.bigint "cocktails_id"
+    t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cocktails_id"], name: "index_reviews_on_cocktails_id"
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
   end
 
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
-  add_foreign_key "reviews", "cocktails", column: "cocktails_id"
+  add_foreign_key "reviews", "cocktails"
 end
